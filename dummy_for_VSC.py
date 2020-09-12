c36df94db82c98a22ef9e04bde2baab48d81b6d5
juan <lopez.hernandez.juan@gmail.com>
2020-09-12 14:28:45 +0200

    Only retained requests

--- dummy_for_VSC.py
+++ dummy_for_VSC.py
@@ -1,34 +1,4 @@
-import sys
-
 import requests
-
-print(sys.version)
-print(sys.executable)
-
-
-def greet(who_to_greet):
-
-    greeting = "Hello, {}".format(who_to_greet)
-    return greeting
-
-
-Juanoto
 
 r = requests.get("https://www.cisco.com")
 print(r.status_code)
-# print(r.headers)
-print(greet("world"))
-# print(greet('Corey'))
-
-name = input("What is your name? ")
-print("Hello, ", name)
-
-# in terminal
-# cls
-# python
-# import sys
-# sys.executable > pyhton path
-# exit()
-# within project folder create a venv : python -m venv venv > creates the venv folder. Pick interpreter in venv. Terminal launches witin venv now.
-# witin venv : pip install requests
-# settings: control space > editor. > all options
