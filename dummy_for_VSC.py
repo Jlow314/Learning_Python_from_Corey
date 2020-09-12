1eb1e27c73a0d75c89f53332c6e91be31765707a
Jlow314 <lopez.hernandez.juan@gmail.com>
2020-09-12 14:48:11 +0200

    Second commit - now with git commands

--- dummy_for_VSC.py
+++ dummy_for_VSC.py
@@ -2,3 +2,8 @@ import requests
 
 r = requests.get("https://www.cisco.com")
 print(r.status_code)
+
+# git: in settings gitbash shell path gezet
+# open terminal opens gitbash shell
+# hierin gitbash configureren : git config --global user.name "Jlow314" en git config --global user.email "lopez.hernandez.juan@gmail.com"
+# git config --list > shows us options and username and email
